b3bb2ef4c3116b0d98a90b058224eba76aae8576
Nolan Eakins <nolan@eakins.net>
2011-10-23 01:08:01 -0400

    Moved the encoding to a constant.

--- lib/mc/string16.rb
+++ lib/mc/string16.rb
@@ -1,7 +1,9 @@
 module MC
   class String16
+    ENCODING = 'UCS-2BE'
+
     def self.serialize(str)
-      [ str.length ].pack('n') + str.encode("UCS-2BE").codepoints.to_a.pack("n#{str.length}")
+      [ str.length ].pack('n') + str.encode(ENCODING).codepoints.to_a.pack("n#{str.length}")
     end
 
     def self.deserialize(io)
@@ -9,7 +11,7 @@ module MC
       len = data.unpack('n')[0]
 
       data = io.read(len * 2)
-      data.force_encoding("BINARY").unpack("n#{len}").inject("".force_encoding("UCS-2BE")) { |a, c| a << c }.encode(Encoding.default_internal || Encoding.default_external)
+      data.force_encoding("BINARY").unpack("n#{len}").inject("".force_encoding(ENCODING)) { |a, c| a << c }.encode(Encoding.default_internal || Encoding.default_external)
     end
   end
 end
